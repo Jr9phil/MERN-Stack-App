4b4e6728fcacb858643284c16e780afd1a5f57b9
jr9 <jr9phil@gmail.com>
2026-01-01 20:52:10 -0600

    mongodb setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import notes from "./routes/notes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001
+
+connectDB();
 
 app.use("/api/notes", notes);
 
-app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
 });
