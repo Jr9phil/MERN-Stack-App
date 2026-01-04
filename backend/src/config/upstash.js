f20509a2d8f4e4d740eb999d3322c45ec4df7837
jr9 <jr9phil@gmail.com>
2026-01-04 15:38:45 -0600

    set rate limit as environment variable

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const RATELIMIT = process.env.RATELIMIT || 10;
+
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "60 s"),
+    limiter: Ratelimit.slidingWindow(RATELIMIT, "60 s"),
     analytics: true,
 });
 
