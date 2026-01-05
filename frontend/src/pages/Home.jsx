35f8afb93ef1623248da3fae88038df68447a9cf
jr9 <jr9phil@gmail.com>
2026-01-05 16:27:15 -0600

    notes not found blurb

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Ascii from "../components/Ascii";
+import NotesNotFound from "../components/NotesNotFound";
 import NoteCard from "../components/NoteCard";
 import RateLimitedUI from "../components/RateLimited";
 import { useEffect, useState } from "react";
@@ -38,6 +38,9 @@ const HomePage = () => {
     return <div className="min-h-screen">
         <Navbar />
         {rateLimited && <RateLimitedUI />}
+
+        {notes.length === 0 && !error && !rateLimited && !loading && <NotesNotFound />}
+
         <div className="max-w-7xl mx-auto p-4 mt-6">
             {loading && <div className="min-h-screen bg-base-200 flex items-center justify-center">
             <LoaderIcon className="animate-spin size-10" />
@@ -53,7 +56,6 @@ const HomePage = () => {
                 </div>
             )}
 
-            {notes.length === 0 && !error && !rateLimited && !loading && <Ascii />}
         </div>
     </div>
 };
